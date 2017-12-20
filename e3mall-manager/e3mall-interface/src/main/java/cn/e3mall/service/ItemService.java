6ea35300b59feb8508fa57e381b6227642f1901e
max <466425213@qq.com>
2017-12-20 11:57:37 +0800

    添加查询所有方法

--- e3mall-manager/e3mall-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3mall-manager/e3mall-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,8 +1,11 @@
 package cn.e3mall.service;
 
+import cn.e3mall.page.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 
 	TbItem findById(Long id);
+	
+	DataGridResult findAll(int page, int rows);
 }
