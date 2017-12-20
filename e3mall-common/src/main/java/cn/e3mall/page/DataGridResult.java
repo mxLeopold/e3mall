c16716af7dbbb35044cc1cd58cb38a11170a0249
max <466425213@qq.com>
2017-12-20 17:15:02 +0800

    实现序列化接口

--- e3mall-common/src/main/java/cn/e3mall/page/DataGridResult.java
+++ e3mall-common/src/main/java/cn/e3mall/page/DataGridResult.java
@@ -1,14 +1,15 @@
 package cn.e3mall.page;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * DataGrid表格数据封装pojo 格式一致，且所有的DataGrid都会用到这种数据类型，所以封装到common工程下
- * 
+ * 且这个类在两个工程中传递，需要实现Serializable序列化接口
  * @author MAXIAO
  *
  */
-public class DataGridResult {
+public class DataGridResult implements Serializable {
 
 	private Long total;
 	private List<?> rows;
