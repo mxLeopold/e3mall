5df331ec22bee0442a2275fd6223518a70a22d56
max <466425213@qq.com>
2017-12-21 21:00:20 +0800

    添加类目选择

---
+++ e3mall-web/src/main/java/cn/e3mall/controller/ItemCatController.java
@@ -0,0 +1,29 @@
+package cn.e3mall.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.e3mall.page.TreeNode;
+import cn.e3mall.service.ItemCatService;
+
+@Controller
+@RequestMapping("/item/cat")
+public class ItemCatController {
+
+	@Autowired
+	private ItemCatService itemCatService;
+
+	@RequestMapping("/list")
+	@ResponseBody
+	public List<TreeNode> getTreeNodeList(@RequestParam(value = "id", defaultValue = "0") Long parentId) {
+		// 根据parentId获得所有分类
+		List<TreeNode> list = itemCatService.getTreeNodeList(parentId);
+		return list;
+	}
+
+}
